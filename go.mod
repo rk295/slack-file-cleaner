7210b6eceb7e3224d1ece584e684e8289ea4de34
Robin Kearney <robin.kearney@ava.uk>
2020-11-12 12:31:29 +0000

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/rk295/slack-file-cleaner
+
+go 1.14
+
+require (
+	github.com/slack-go/slack v0.7.2
+	go.uber.org/zap v1.16.0
+)
---
+++ main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/slack-go/slack"
+	"go.uber.org/zap"
+)
+
+const (
+	daysOld     = 120 // Number of days to keep files for
+	saveDir     = "files"
+	tokenEnvVar = "TOKEN"
+)
+
+type server struct {
+	log   *zap.SugaredLogger
+	slack *slack.Client
+}
+
+func main() {
+
+	s := &server{}
+
+	logger, _ := zap.NewDevelopment()
+	defer logger.Sync()
+	log := logger.Sugar()
+	s.log = log
+
+	slackToken := os.Getenv(tokenEnvVar)
+	if slackToken == "" {
+		s.log.Errorf("%s env var must be set", tokenEnvVar)
+		os.Exit(1)
+	}
+
+	api := slack.New(slackToken)
+	s.slack = api
+
+	s.listFiles()
+}
+
+func (s *server) listFiles() {
+
+	now := time.Now()
+	day := 24 * time.Hour
+	oneMonth := now.Add(-daysOld * day)
+
+	to := slack.JSONTime(oneMonth.Unix())
+
+	params := slack.GetFilesParameters{
+		Count:       1000,
+		TimestampTo: to,
+	}
+
+	// lazzily ignoring pagination. It'll run every night, we don't upload 1000
+	// a day so it will eventually catch up.
+	files, _, err := s.slack.GetFiles(params)
+
+	if err != nil {
+		s.log.Error(err)
+		return
+	}
+
+	s.log.Infof("found %v files for deletion", len(files))
+
+	for _, file := range files {
+
+		err := s.getFile(file)
+		if err != nil {
+			s.log.Infof("error saving file %s: %s", file.ID, err)
+			// If we couldn't save if, don't delete it
+			continue
+		}
+
+		err = s.slack.DeleteFile(file.ID)
+		if err != nil {
+			s.log.Infof("delete of %s failed: %s", file.ID, err)
+		}
+	}
+}
+
+func (s *server) getFile(file slack.File) error {
+	filename := fmt.Sprintf("%s-%s", file.ID, file.Name)
+	year, month, day := file.Timestamp.Time().Date()
+	datePath := fmt.Sprintf("%v/%02d/%v", year, month, day)
+
+	dir := filepath.Join(saveDir, datePath)
+	fullFilePath := filepath.Join(dir, filename)
+
+	s.log.Debugf("id=%s name=%s Timestamp=%s fullFilePath=%s", file.ID, file.Name, file.Timestamp, fullFilePath)
+
+	err := os.MkdirAll(dir, 0755)
+	if err != nil {
+		return err
+	}
+
+	f, err := os.Create(fullFilePath)
+	if err != nil {
+		return err
+	}
+
+	err = s.slack.GetFile(file.URLPrivateDownload, f)
+	if err != nil {
+		return err
+	}
+
+	err = f.Close()
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
