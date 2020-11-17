c1b870d428c05ceb060b5ff134d29cd90b3812c0
Robin Kearney <robin@kearney.co.uk>
2020-11-17 11:49:37 +0000

    Minor updates

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -11,7 +12,7 @@ import (
 )
 
 const (
-	daysOld     = 120 // Number of days to keep files for
+	daysOld     = 90 // Number of days to keep files for
 	saveDir     = "files"
 	tokenEnvVar = "TOKEN"
 )
@@ -30,6 +31,8 @@ func main() {
 	log := logger.Sugar()
 	s.log = log
 
+	s.log.Info("starting")
+
 	slackToken := os.Getenv(tokenEnvVar)
 	if slackToken == "" {
 		s.log.Errorf("%s env var must be set", tokenEnvVar)
@@ -39,14 +42,16 @@ func main() {
 	api := slack.New(slackToken)
 	s.slack = api
 
-	err := s.listFiles()
+	ctx := context.Background()
+
+	err := s.listFiles(ctx)
 	if err != nil {
 		s.log.Error(err)
 		os.Exit(1)
 	}
 }
 
-func (s *server) listFiles() error {
+func (s *server) listFiles(ctx context.Context) error {
 
 	now := time.Now()
 	day := 24 * time.Hour
@@ -57,10 +62,11 @@ func (s *server) listFiles() error {
 	params := slack.GetFilesParameters{
 		Count:       1000,
 		TimestampTo: to,
+		ShowHidden:  true,
 	}
 
 	// lazzily ignoring pagination. It'll run every night, we don't upload 1000
-	// a day so it will eventually catch up.
+	// a day so it will eventuatiglly catch up.
 	files, _, err := s.slack.GetFiles(params)
 	if err != nil {
 		return err
@@ -68,6 +74,7 @@ func (s *server) listFiles() error {
 
 	fileCount := len(files)
 	if fileCount == 0 {
+		s.log.Infof("found no files to delete that were older than %d", daysOld)
 		return nil
 	}
 
@@ -75,23 +82,49 @@ func (s *server) listFiles() error {
 
 	for _, file := range files {
 
-		err := s.getFile(file)
-		if err != nil {
-			s.log.Infof("error saving file %s: %s", file.ID, err)
-			// If we couldn't save if, don't delete it
-			continue
+		if file.Mode == "hidden_by_limit" {
+			s.log.Infof("file id %s is hidden by free quota limit, won't download before deleting", file.ID)
+		} else {
+			err := s.getFile(file)
+			if err != nil {
+				s.log.Infof("error saving file %s: %s", file.ID, err)
+				continue
+			}
 		}
 
-		err = s.slack.DeleteFile(file.ID)
+		err := s.deleteFile(ctx, file.ID)
 		if err != nil {
-			s.log.Infof("delete of %s failed: %s", file.ID, err)
+			s.log.Info(err)
 		}
 	}
 
 	return nil
 }
 
+func (s *server) deleteFile(ctx context.Context, fileID string) (err error) {
+	for err == nil {
+		err := s.slack.DeleteFile(fileID)
+		if err == nil {
+			return err
+		} else if rateLimitedError, ok := err.(*slack.RateLimitedError); ok {
+			select {
+			case <-ctx.Done():
+				err = ctx.Err()
+			case <-time.After(rateLimitedError.RetryAfter):
+				err = nil
+			}
+		}
+	}
+	return err
+}
+
 func (s *server) getFile(file slack.File) error {
+
+	if file.URLPrivateDownload == "" {
+		s.log.Debugf("URLPrivateDownload for %s is empty, skipping", file.ID)
+		return nil
+	}
+
 	filename := fmt.Sprintf("%s-%s", file.ID, file.Name)
 	year, month, day := file.Timestamp.Time().Date()
 	datePath := fmt.Sprintf("%v/%02d/%v", year, month, day)
@@ -99,7 +132,7 @@ func (s *server) getFile(file slack.File) error {
 	dir := filepath.Join(saveDir, datePath)
 	fullFilePath := filepath.Join(dir, filename)
 
-	s.log.Debugf("id=%s name=%s Timestamp=%s fullFilePath=%s", file.ID, file.Name, file.Timestamp, fullFilePath)
+	s.log.Debugf("file_id=%s user_name=%s name=%s Timestamp=%s fullFilePath=%s", file.ID, s.getUser(file.User), file.Name, file.Timestamp, fullFilePath)
 
 	err := os.MkdirAll(dir, 0755)
 	if err != nil {
@@ -123,4 +156,16 @@ func (s *server) getFile(file slack.File) error {
 
 	return nil
 
+}
+
+func (s *server) getUser(userID string) string {
+
+	user, err := s.slack.GetUserInfo(userID)
+
+	if err != nil {
+		s.log.Warnf("error fetching user details for user_id=%s error:%s", userID, err)
+		return "user-lookup-failed"
+	}
+
+	return user.Name
 }
